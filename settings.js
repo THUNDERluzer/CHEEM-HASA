2129eacd4e2ac466672e7de9bf23221dc78fd839
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2023-10-19 16:10:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 //aumto presence update
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
@@ -35,10 +35,10 @@ global.ownername = 'Hashan'
 global.ownerNumber = ["94782543893@s.whatsapp.net"]
 global.ownerweb = "wa.me94782543893"
 global.websitex = "wa.me94782543893"
-global.wagc = "wa.me94782543893"
+global.wagc = "wa.me/94782543893"
 global.themeemoji = '🪀'
 global.wm = "Hashan"
-global.botscript = 'wa.me94782543893' //script link
+global.botscript = 'wa.me/94782543893' //script link
 global.packname = "Sticker By"
 global.author = "Hashan\n\n"
 global.creator = "94782543893@s.whatsapp.net"
